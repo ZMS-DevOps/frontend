37e76d23e1ca0d882a343849d7c355ce14eb9757
Zorica Vuković <90617418+zoricavukovic@users.noreply.github.com>
2024-05-26 19:26:47 +0200

    feat: Login (#2)
    * feat: Login
    
    * fix: remove empty ngOnInit in login.component.ts

---
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from "./pages/login/login.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {AuthRoutes} from "./auth.routes";
+import {CustomMaterialModuleModule} from "../../custom-material-module/custom-material-module.module";
+import {NgxStarsModule} from "ngx-stars";
+import {SharedModule} from "../shared/shared.module";
+import {CarouselModule} from "primeng/carousel";
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CustomMaterialModuleModule,
+    RouterModule.forChild(AuthRoutes),
+    NgxStarsModule,
+    SharedModule,
+    CarouselModule,
+  ],
+  providers: [],
+})
+export class AuthModule { }
---
+++ frontend/src/app/modules/auth/pages/login/login.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription} from 'rxjs';
+import {AuthService} from "../../services/auth.service";
+import { ToastrService } from 'ngx-toastr';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnDestroy {
+  loginForm: FormGroup;
+
+  hide = true;
+  authSubscription: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private toast: ToastrService,
+    private router: Router,
+  ) {
+    this.loginForm = this.getEmptyForm();
+  }
+
+  getErrorMessage() {
+    if (this.loginForm.get('email').hasError('required')) {
+      return 'Email is required';
+    }
+
+    return this.loginForm.get('email').hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
+
+  login() {
+    this.authService.login(this.loginForm.getRawValue()).subscribe({
+      next: loggedUser => {
+
+        this.authService.setLocalStorage(loggedUser);
+        this.router.navigate(['/booking/home-page']);
+      },
+      error: err => {
+        console.error(err);
+        this.toast.error('Account invalid credentials!', 'Login failed');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
+
+  private getEmptyForm() {
+    return new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{12,}$"),
+      ]),
+    });
+  }
+}
